db4b6807b32e5dea5036dcf1e2a2402417ae56a3
r3surr3cti0n <matraattila2004@gmail.com>
2021-06-05 12:05:46 +0200

    1. Generate divs with classes in a better form. 2. Set the div's background color to blue on mouseover event.

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+// 16x16 div
+const divs = 256;
+
 window.addEventListener("DOMContentLoaded", createDivs);
 
 function createDivs() {
 	const container = document.querySelector(".container");
 
-	for (let i = 0; i < 256; i++) {
-		container.innerHTML += "<div class='square'></div>";
+	for (let i = 0; i < divs; i++) {
+		let div = document.createElement("div");
+		div.setAttribute("class", "square");
+		container.append(div);
+		div.addEventListener("mouseover", setColor);
 	}
+}
+
+function setColor(e) {
+	e.target.style.background = "blue";
 }
