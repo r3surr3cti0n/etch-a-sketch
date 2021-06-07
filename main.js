33e555da0ff4b18e91fa19a5378040625198256f
r3surr3cti0n <matraattila2004@gmail.com>
2021-06-07 12:11:19 +0200

    Add random color generator

--- main.js
+++ main.js
@@ -39,3 +39,15 @@ function setColor(e) {
 function Clear() {
 	container.innerHTML = "";
 }
+
+// Generate a random color in hexadecimal notation
+function randomColor() {
+	let color = "#";
+
+	for (let i = 0; i < 6; i++) {
+		const random = Math.random();
+		const bit = (random * 16) | 0;
+		color += bit.toString(16);
+	}
+	return color;
+}
